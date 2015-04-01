5e6e201cd5253c6608622395306be8aa1ffb2bbc
rcodonnell <rcoreyodonnell@gmail.com>
2015-04-01 00:13:49 -0400

    fixed spelling and broken link

--- contact/index.php
+++ contact/index.php
@@ -8,14 +8,7 @@
     <link href="http://www.rcodonnell.com/resources/css/style.css" rel="stylesheet">
     <!--<link href="../resources/css/style.css" rel="stylesheet">-->
 
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-52350725-1', 'auto');
-      ga('send', 'pageview');
-    </script>
+
 
   </head>
 
@@ -186,5 +179,15 @@
         </span>
       </div>
     </footer>
+
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-52350725-1', 'auto');
+      ga('send', 'pageview');
+    </script>
 </body>
 </html>
